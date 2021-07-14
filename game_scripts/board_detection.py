ef495ed7515e47b6176fd6f33f1ee9b818c33183
LuisC18 <martinez.737@osu.edu>
2021-07-14 13:10:21 -0400

    Game scripts folder

---
+++ game_scripts/board_detection.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from robot_support import *
+
+## scan board 
+
+## wait for human move or play move
+
+## scan board and think of best move
+
+### starting X and O in stacked same place (code recurrsive pickup every move with decreasing z values for stack)
+## pickup from stack
+
+## play move
+
+def main():
+  try:
+    while gameInProgress == True:
+
+  except rospy.ROSInterruptException:
+    exit()
+  except KeyboardInterrupt:
+    exit()
+
+
+if __name__ == '__main__':
+  main()
+
+
+ 
