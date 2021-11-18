07c240c6d51cc2295f148cb1b4328d38ab7414bd
LuisC18 <martinez.737@osu.edu>
2021-11-18 18:18:27 -0500

    Initial Refactor/Camera Intrinsics
    *WIP camera intrinsic, able to output center pixel depth, ROS issue with subscribers might transition to a function that contains waitformessage calls instead
    *Created master script tictactoe.py

--- scripts/image_process.py
+++ scripts/image_process.py
@@ -7,9 +7,10 @@
 # Author: LuisC18
 
 ## IMPORTS
+import pyrealsense2 as rs
 import rospy
 from cv_bridge import CvBridge, CvBridgeError
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 import numpy as np
 
 class ImageProcess(object):
@@ -18,39 +19,68 @@ class ImageProcess(object):
     """
 
     def __init__(self):
+        self.bridge = CvBridge()
 
         self.color_image_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.color_image_callback)
         self.aligned_depth_image_sub = rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image, self.alighed_depth_image_callback)
+        self.intrinsics_sub = rospy.Subscriber("/camera/aligned_depth_to_color/camera_info",CameraInfo, self.intrinsics_callback)
 
-    def color_image_callback(self,msg):
-        self.color_image = self.bridge.imgmsg_to_cv2(msg.data, "bgr8")
-
-    def alighed_depth_image_callback(self, msg):
-
+    def color_image_callback(self,color_image_msg):
+        """
+            ROS subscriber callback that converts the RGB stream sensor_msgs/Image Message to an openCV image and stores it in the self.color_image variable.
+        """
         try:
-            depth_image = bridge.imgmsg_to_cv2(msg.data, desired_encoding="passthrough")
-            depth_array = np.array(depth_image, dtype=np.float32)
-            center_idx = np.array(depth_array.shape) / 2
-            print('center depth:', depth_array[center_idx[0], center_idx[1]])
+            self.color_image = self.bridge.imgmsg_to_cv2(color_image_msg, "bgr8")
 
         except CvBridgeError, e:
             print e
 
-
-    def intrinsics(self):
-
-    def detectBoard_contours(image):
+    def alighed_depth_image_callback(self, depth_image_msg):
         """
-            Tictactoe function that finds the physical board. Utilizes ShapeDetector class functions.
-            @param image: image parameter the function tries to find the board on.
-            @return scaledCenter: (x ,y) values in meters of the board center relative to the center of the camera frame.
-            @return boardImage: image with drawn orientation axes and board location.
-            @return tf_camera2board: transformation matrix of board to camera frame of reference.
-            """
+            ROS subscriber callback that converts the depth stream sensor_msgs/Image Message to an openCV image and stores it in the self.color_image variable.
+        """
+        try:
+            self.depth_image = self.bridge.imgmsg_to_cv2(depth_image_msg, desired_encoding="passthrough")
+            
+        except CvBridgeError, e:
+            print e
+
+    def intrinsics_callback(self,camera_info_msg):
+
+        self.camera_intrinsic = np.array(camera_info_msg)
+
+    def depth_at_center_pixel(self):
+        depth_array = np.array(self.depth_image, dtype=np.float32)
+        center_idx = np.array(depth_array.shape) / 2
+        center_pixel = (center_idx[0], center_idx[1])
+        center_pixel_depth = depth_array[center_pixel]
+        depth_point_in_meters_camera_cords = rs.rs2_deproject_pixel_to_point(self.camera_intrinsic, center_pixel, center_pixel_depth)
+        print(depth_point_in_meters_camera_cords)
+
+    
+    
+    
 
 
+
+    # def detectBoard_contours(image):
+    #     """
+    #         Tictactoe function that finds the physical board. Utilizes ShapeDetector class functions.
+    #         @param image: image parameter the function tries to find the board on.
+    #         @return scaledCenter: (x ,y) values in meters of the board center relative to the center of the camera frame.
+    #         @return boardImage: image with drawn orientation axes and board location.
+    #         @return tf_camera2board: transformation matrix of board to camera frame of reference.
+    #         """
+
+
+
+
+
+def main():
+    rospy.init_node('image_processor', anonymous=False)
+    IP = ImageProcess()
+    IP.depth_at_center_pixel()
+    rospy.spin()
 
 if __name__ == '__main__':
-    rospy.init_node('image_processor', anonymous=False)
-    ImageProcess()
-    rospy.spin()
+    main()
