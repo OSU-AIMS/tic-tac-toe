1fbc7f769ded23618d97122d9a424fe59ff348a9
Mohammad Khan <mohammadukhan135@gmail.com>
2021-11-22 14:17:31 -0500

    HeatMap: Filter2D& MatchTemplate
    - filter2D doesn't generate gradient
    - matchtemplate genreates gradient but only works in limited scenarios where object is in the same orientation, lighting, as template

--- develop/image_convolution/test_convolutions.py
+++ develop/image_convolution/test_convolutions.py
@@ -14,15 +14,17 @@ def kernel_runner(image):
     # Create kernel (format - "BGR")
     kernel_size = 3
     kernel = np.dstack((255 * np.ones((kernel_size, kernel_size, 1), dtype='uint8'),np.zeros((kernel_size, kernel_size, 2), dtype='uint8')))
-    # Changed np.zeros(kernel_size,kernel_size,3)  to np.zeros(kernel_size,kernel_size,2) so kernel is 3 channels
+    # 11/22: changed dtype from uinut8 to np.float32 --> didn't change anything
+    # Changed np.zeros(kernel_size,kernel_size,3)  to np.zeros(kernel_size,kernel_size,2) so kernel is 3 channels instead of 4
     # format: BGR
     # numpy dstack docs: https://numpy.org/doc/stable/reference/generated/numpy.dstack.html
     kernel_b = 255 * np.ones((kernel_size, kernel_size), dtype='uint8')
+    print('Kernel Matrix: 3x3x1')
     print((kernel_b))
 
 
     # Method: Filter2D
-<<<<<<< Updated upstream
+# <<<<<<< Updated upstream
     # dst = image.copy()
     # output = cv2.filter2D(src=image, dst=dst, ddepth=-1, kernel=kernel_b)
     # cv2.imshow('Input Array',image)
@@ -32,15 +34,16 @@ def kernel_runner(image):
     # cv2.imwrite("output.tiff", output)
     # wouldn't output = dst??
     # 11/18: Result is not a gradient
+    # kernel_b=cv2.flip(kernel_b,-1)
+    # dst = image.copy()
+    # image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # output = cv2.filter2D(src=image, ddepth=-1, kernel=kernel_b)
+    # 11/22: removed dst=dst
+    # cv2.imshow('Heatmap',output)
+    # cv2.imshow('Output Array dst', dst)
+    # cv2.imwrite('dst.tiff', dst) dst & output are the same
+    # cv2.imwrite('output_filter2D.tif', output)
 
-=======
-    dst = image.copy()
-    output = cv2.filter2D(src=image, dst=dst, ddepth=-1, kernel=kernel_b)
-    cv2.imshow('Heatmap',output)
-    cv2.imshow('Output Array dst', dst)
-    cv2.imwrite('dst.tiff', dst)
-    cv2.imwrite('output.tiff', output)
->>>>>>> Stashed changes
     '''
     filter2D parameters:
         InputArray src, 
@@ -53,28 +56,32 @@ def kernel_runner(image):
     # opencv docs on filter2D:
     # https://docs.opencv.org/4.2.0/d4/d86/group__imgproc__filter.html#ga27c049795ce870216ddfb366086b5a04
     '''
-    
-    # # Method: Template Matching
-    # image = image[:, :, 0]
+
+ # Uncomment below after testing out filter2D function & get a consistent output
+
+    # # # Method: Template Matching
+    # # image = image[:, :, 0]
     img_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    # resb = cv2.matchTemplate(image=image[:, :, 0], templ=kernel_b, method=1)
-    # resg = cv2.matchTemplate(image=image[:, :, 1], templ=kernel_b, method=1)
-    # resr = cv2.matchTemplate(image=image[:, :, 2], templ=kernel_b, method=1)
-    # cv2.imwrite('resb.tiff',resb)
-    # cv2.imwrite('resg.tiff',resg)
-    # cv2.imwrite('resr.tiff',resr)
-    # template = kernel_b[1,:,:]
+    # # resb = cv2.matchTemplate(image=image[:, :, 0], templ=kernel_b, method=1)
+    # # resg = cv2.matchTemplate(image=image[:, :, 1], templ=kernel_b, method=1)
+    # # resr = cv2.matchTemplate(image=image[:, :, 2], templ=kernel_b, method=1)
+    # # cv2.imwrite('resb.tiff',resb)
+    # # cv2.imwrite('resg.tiff',resg)
+    # # cv2.imwrite('resr.tiff',resr)
+    # # template = kernel_b[1,:,:]
     '''
     matchTemplate docs
     https://docs.opencv.org/4.2.0/df/dfb/group__imgproc__object.html#gga3a7850640f1fe1f58fe91a2d7583695dac6677e2af5e0fae82cc5339bfaef5038
         Input Array: image (must be 8 bit or 32 bit floating point)
         Input array: Templ (serached template)
        output array: result
-                int: method (https://docs.opencv.org/4.2.0/df/dfb/group__imgproc__object.html#ga3a7850640f1fe1f58fe91a2d7583695d)
+               int: method (https://docs.opencv.org/4.2.0/df/dfb/group__imgproc__object.html#ga3a7850640f1fe1f58fe91a2d7583695d)
                mask: mask of serached template. Same datatype & size as templ. Not set by default
     '''
-    res = cv2.matchTemplate(image=img_gray,templ=kernel_b,method=1)
-    cv2.imwrite('res.tiff',res)
+
+
+    res = cv2.matchTemplate(image=img_gray,templ=kernel_b,method=3)
+    cv2.imwrite('res_match_template.tiff',res)
     
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
     print('min_val')
@@ -94,13 +101,13 @@ def kernel_runner(image):
     endY = startY + kernel_b.shape[0]
     
     # draw the bounding box on the image
-    cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 3)
+    b_box_image = cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 1)
     # show the output image
-    cv2.imshow("Output", image)
+    # cv2.imshow("Output based on matchTemplate", b_box_image)
+    cv2.imwrite('res_match_template_BoundingBox.tiff',b_box_image)
     cv2.waitKey(0)
 
-
-    '''
+'''
     cv::TemplateMatchModes 
     cv::TM_SQDIFF = 0,
     cv::TM_SQDIFF_NORMED = 1,
@@ -108,7 +115,7 @@ def kernel_runner(image):
     cv::TM_CCORR_NORMED = 3,
     cv::TM_CCOEFF = 4,
     cv::TM_CCOEFF_NORMED = 5
-    '''
+'''
 
 
 
@@ -123,6 +130,7 @@ def kernel_runner(image):
 
 if __name__ == '__main__':
     print("Your OpenCV version is: " + cv2.__version__)  
-    image = cv2.imread("images_10x10/test_box.tif")
+    image = cv2.imread("images_10x10/test_angle.tif")
+    # image = cv2.imread("images_20x20/test_box_20x20.tif")
     # image = image[:, :, 0]
     kernel_runner(image.copy())
