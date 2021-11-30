57067efb0c2ae1084e51775c921d2666d8d45a92
LuisC18 <martinez.737@osu.edu>
2021-11-30 13:58:02 -0500

    image_process: CameraInfo msg to realsense intrinsic conversion
    *changed subscribers to wait_for_messages
    *converted cameraInfo ros message to pyrealsense2 intrinsic for deprojection

--- scripts/image_process.py
+++ scripts/image_process.py
@@ -20,48 +20,67 @@ class ImageProcess(object):
 
     def __init__(self):
         self.bridge = CvBridge()
+        self.listener()
 
-        self.color_image_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.color_image_callback)
-        self.aligned_depth_image_sub = rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image, self.alighed_depth_image_callback)
-        self.intrinsics_sub = rospy.Subscriber("/camera/aligned_depth_to_color/camera_info",CameraInfo, self.intrinsics_callback)
+    def listener(self):
+        color_image_msg = rospy.wait_for_message("/camera/color/image_raw", Image, timeout=None)
+        aligned_depth_image_msg = rospy.wait_for_message("/camera/aligned_depth_to_color/image_raw", Image, timeout=None)
+        self.camera_info_msg = rospy.wait_for_message("/camera/aligned_depth_to_color/camera_info", CameraInfo, timeout=None)
 
-    def color_image_callback(self,color_image_msg):
-        """
-            ROS subscriber callback that converts the RGB stream sensor_msgs/Image Message to an openCV image and stores it in the self.color_image variable.
-        """
         try:
             self.color_image = self.bridge.imgmsg_to_cv2(color_image_msg, "bgr8")
-
+            self.depth_image = self.bridge.imgmsg_to_cv2(aligned_depth_image_msg, desired_encoding="passthrough")
         except CvBridgeError, e:
             print e
 
-    def alighed_depth_image_callback(self, depth_image_msg):
-        """
-            ROS subscriber callback that converts the depth stream sensor_msgs/Image Message to an openCV image and stores it in the self.color_image variable.
-        """
-        try:
-            self.depth_image = self.bridge.imgmsg_to_cv2(depth_image_msg, desired_encoding="passthrough")
-            
-        except CvBridgeError, e:
-            print e
+    def camera_info_to_intrinsics(self,cameraInfo):
 
-    def intrinsics_callback(self,camera_info_msg):
+        intrinsics = rs.intrinsics()
 
-        self.camera_intrinsic = np.array(camera_info_msg)
+        intrinsics.width = cameraInfo.width
+        intrinsics.height = cameraInfo.height
+        intrinsics.ppx = cameraInfo.K[2]
+        intrinsics.ppy = cameraInfo.K[5]
+        intrinsics.fx = cameraInfo.K[0]
+        intrinsics.fy = cameraInfo.K[4]
+        # intrinsics.model = cameraInfo.distortion_model
+        intrinsics.model  = rs.distortion.none     
+        intrinsics.coeffs = [i for i in cameraInfo.D]
 
+        return intrinsics
+        
     def depth_at_center_pixel(self):
+        self.listener()
+
         depth_array = np.array(self.depth_image, dtype=np.float32)
         center_idx = np.array(depth_array.shape) / 2
         center_pixel = (center_idx[0], center_idx[1])
+        
         center_pixel_depth = depth_array[center_pixel]
-        depth_point_in_meters_camera_cords = rs.rs2_deproject_pixel_to_point(self.camera_intrinsic, center_pixel, center_pixel_depth)
+
+        return center_pixel_depth
+
+
+    def depth_at_pixel(self,x,y):
+        self.listener()
+
+        depth_array = np.array(self.depth_image, dtype=np.float32)
+        pixel = (x, y)
+
+        pixel_depth = depth_array[pixel]
+
+        return pixel_depth
+
+    def convert_depth_to_phys_coord(self,x,y):
+        self.listener()
+
+        depth = self.depth_at_pixel(x,y)
+        intrinsics = self.camera_info_to_intrinsics(self.camera_info_msg)
+
+        depth_point_in_meters_camera_cords = rs.rs2_deproject_pixel_to_point(intrinsics, [x,y], depth)
         print(depth_point_in_meters_camera_cords)
-
-    
-    
-    
-
-
+        # print("y:",-depth_point_in_meters_camera_cords[0])
+        # print("z:",-depth_point_in_meters_camera_cords[1])
 
     # def detectBoard_contours(image):
     #     """
@@ -73,14 +92,13 @@ class ImageProcess(object):
     #         """
 
 
-
-
-
 def main():
     rospy.init_node('image_processor', anonymous=False)
     IP = ImageProcess()
-    IP.depth_at_center_pixel()
-    rospy.spin()
+    # IP.depth_at_center_pixel()
+    # table estimation  ~ 81 cm, width ~ 60cm
+    IP.convert_depth_to_phys_coord(320,100)
+    
 
 if __name__ == '__main__':
     main()
