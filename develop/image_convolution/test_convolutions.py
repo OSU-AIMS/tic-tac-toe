f25d10bb07714dfe0780235d16831dc7967e6b92
Mohammad Khan <mohammadukhan135@gmail.com>
2021-12-14 10:47:00 -0500

    matchTemplate to recognize blue based on RGB
    - Not successful yet, tried making a depth 3 array of (0,0,255) and tried (255,0,0)
    - Need to check if MatchTemplate can be used with a depth 3 array or just a depth 1 array

--- develop/image_convolution/test_convolutions.py
+++ develop/image_convolution/test_convolutions.py
@@ -12,8 +12,12 @@ import numpy as np
 
 def kernel_runner(image):
     # Create kernel (format - "BGR")
-    kernel_size = 3
-    kernel = np.dstack((255 * np.ones((kernel_size, kernel_size, 1), dtype='uint8'),np.zeros((kernel_size, kernel_size, 2), dtype='uint8')))
+    kernel_size = 5
+    print('Image Parameter')
+    # print(image)
+    print('Shape of image')
+    print(np.shape(image))
+    # kernel = np.dstack((255 * np.ones((kernel_size, kernel_size, 1), dtype='uint8'),np.zeros((kernel_size, kernel_size, 2), dtype='uint8')))
     # 11/22: changed dtype from uinut8 to np.float32 --> didn't change anything
     # Changed np.zeros(kernel_size,kernel_size,3)  to np.zeros(kernel_size,kernel_size,2) so kernel is 3 channels instead of 4
     # format: BGR
@@ -22,15 +26,30 @@ def kernel_runner(image):
     # Uncomment below to use white kernel 3x3x3
     # kernel_b = 255 * np.ones((kernel_size, kernel_size), dtype='uint8')
 
+    # Uncomment below to create blue array
+    ch1 = 255*np.ones((kernel_size, kernel_size), dtype='uint8')
+    ch2 = np.zeros((kernel_size, kernel_size), dtype='uint8')
+    kernel_b = np.array([ch2, ch2, ch1], ndmin=3, dtype='uint8')
+    # kernel_b = np.stack((ch2,ch2,ch1),axis=-1)
+    # 12/6: issue with creating 3 channel array
+    # need (0,0,255) 3 channel array
+    # Currently makes [0,0,255],[0,0,255]. Need [0,0,0],[0,0,0],[255,255,255]
+
+
     # Attempting to Create a blue (255,0,0) 3x3x3 Kernel
     # kernel_b = np.dstack((255 * np.ones((kernel_size, kernel_size,1), dtype='uint8'), np.zeros((kernel_size, kernel_size, 2), dtype='uint8')))
-    kernel_b = cv2.imread('tic_tac_toe_images/blue_square_crop.tiff')
 
-    kernel_r = cv2.imread('tic_tac_toe_images/red_square_crop.tiff')
 
-    kernel_g = cv2.imread('tic_tac_toe_images/green_square_crop.tiff')
 
-    # print('Kernel Matrix: 3x3x1')
+    # Uncomment below to use square images as kernels
+    # kernel_b = cv2.imread('tic_tac_toe_images/blue_square_crop.tiff')
+
+    # kernel_r = cv2.imread('tic_tac_toe_images/red_square_crop.tiff')
+
+    # kernel_g = cv2.imread('tic_tac_toe_images/green_square_crop.tiff')
+
+    print('Kernel Matrix: should be 3x3x3')
+    print(np.shape(kernel_b)) # returns 3x3x3
     # print(kernel_b)
 
 
@@ -92,6 +111,8 @@ def kernel_runner(image):
 
     # # Recognizing Blue Square
     res_B = cv2.matchTemplate(image=image,templ=kernel_b,method=5)
+    # Use method=5 when using the square images as kernels
+    # Use method= when using arrays as kernels
     cv2.imwrite('res_match_template_B.tiff',res_B)
     min_val_B, max_val_B, min_loc_B, max_loc_B = cv2.minMaxLoc(res_B)
     # print('min_val_B')
@@ -111,65 +132,65 @@ def kernel_runner(image):
     endY_B = startY_B + kernel_b.shape[0]
 
     # draw the bounding box on the image
-    b_box_image = cv2.rectangle(image, (startX_B, startY_B), (endX_B, endY_B), (255, 0, 0), 3)
+    b_box_image = cv2.rectangle(image, (startX_B, startY_B), (endX_B, endY_B), (255, 0, 0), 4) # BGR for openCV
     # show the output image
     # cv2.imshow("Output based on matchTemplate", b_box_image)
     cv2.imwrite('res_match_template_Blue_BoundingBox.tiff', b_box_image)
     # cv2.waitKey(0)
 
     #### Recognizing Red Square
-    res_R = cv2.matchTemplate(image=image,templ= kernel_r,method=5)
-    cv2.imwrite('res_match_template_R.tiff', res_R)
-    min_val_R, max_val_R, min_loc_R, max_loc_R = cv2.minMaxLoc(res_R)
-    # print('min_val_R')
-    # print(min_val_R)
-    # print('max_val_R')
-    # print(max_val_R)
-    print('min_loc_R')
-    print(min_loc_R)
-    print('max_loc_R')
-    print(max_loc_R)
-
-    # Drawing Bounding Box around detected shape
-    # determine the starting and ending (x, y)-coordinates of the bounding box
-    # From: https://www.pyimagesearch.com/2021/03/22/opencv-template-matching-cv2-matchtemplate/
-    (startX_R, startY_R) = max_loc_R
-    endX_R = startX_R + kernel_r.shape[1]
-    endY_R = startY_R + kernel_r.shape[0]
-
-    # draw the bounding box on the image
-    r_box_image = cv2.rectangle(image, (startX_R, startY_R), (endX_R, endY_R), (0, 0, 255), 3)
-    # show the output image
-    # cv2.imshow("Output based on matchTemplate", r_box_image)
-    cv2.imwrite('res_match_template_RED_BoundingBox.tiff', r_box_image)
-    # cv2.waitKey(0)
+    # res_R = cv2.matchTemplate(image=image,templ= kernel_r,method=5)
+    # cv2.imwrite('res_match_template_R.tiff', res_R)
+    # min_val_R, max_val_R, min_loc_R, max_loc_R = cv2.minMaxLoc(res_R)
+    # # print('min_val_R')
+    # # print(min_val_R)
+    # # print('max_val_R')
+    # # print(max_val_R)
+    # print('min_loc_R')
+    # print(min_loc_R)
+    # print('max_loc_R')
+    # print(max_loc_R)
+    #
+    # # Drawing Bounding Box around detected shape
+    # # determine the starting and ending (x, y)-coordinates of the bounding box
+    # # From: https://www.pyimagesearch.com/2021/03/22/opencv-template-matching-cv2-matchtemplate/
+    # (startX_R, startY_R) = max_loc_R
+    # endX_R = startX_R + kernel_r.shape[1]
+    # endY_R = startY_R + kernel_r.shape[0]
+    #
+    # # draw the bounding box on the image
+    # r_box_image = cv2.rectangle(image, (startX_R, startY_R), (endX_R, endY_R), (0, 0, 255), 3)
+    # # show the output image
+    # # cv2.imshow("Output based on matchTemplate", r_box_image)
+    # cv2.imwrite('res_match_template_RED_BoundingBox.tiff', r_box_image)
+    # # cv2.waitKey(0)
 
     #### Recognizing Green Square
-    res_G = cv2.matchTemplate(image=image,templ= kernel_g,method=5)
-    cv2.imwrite('res_match_template_G.tiff', res_G)
-    min_val_G, max_val_G, min_loc_G, max_loc_G = cv2.minMaxLoc(res_G)
-    # print('min_val_G')
-    # print(min_val_G)
-    # print('max_val_G')
-    # print(max_val_G)
-    print('min_loc_G')
-    print(min_loc_G)
-    print('max_loc_G')
-    print(max_loc_G)
-
-    # Drawing Bounding Box around detected shape
-    # determine the starting and ending (x, y)-coordinates of the bounding box
-    # From: https://www.pyimagesearch.com/2021/03/22/opencv-template-matching-cv2-matchtemplate/
-    (startX_G, startY_G) = max_loc_G
-    endX_G = startX_G + kernel_g.shape[1]
-    endY_G = startY_G + kernel_g.shape[0]
-
-    # draw the bounding box on the image
-    g_box_image = cv2.rectangle(image, (startX_G, startY_G), (endX_G, endY_G), (0, 255, 0), 3)
-    # show the output image
-    # cv2.imshow("Output based on matchTemplate", g_box_image)
-    cv2.imwrite('res_match_template_GREEN_BoundingBox.tiff', g_box_image)
-    # cv2.waitKey(0)
+    # res_G = cv2.matchTemplate(image=image,templ= kernel_g,method=5)
+    # cv2.imwrite('res_match_template_G.tiff', res_G)
+    # min_val_G, max_val_G, min_loc_G, max_loc_G = cv2.minMaxLoc(res_G)
+    # # print('min_val_G')
+    # # print(min_val_G)
+    # # print('max_val_G')
+    # # print(max_val_G)
+    # print('min_loc_G')
+    # print(min_loc_G)
+    # print('max_loc_G')
+    # print(max_loc_G)
+    #
+    # # Drawing Bounding Box around detected shape
+    # # determine the starting and ending (x, y)-coordinates of the bounding box
+    # # From: https://www.pyimagesearch.com/2021/03/22/opencv-template-matching-cv2-matchtemplate/
+    # (startX_G, startY_G) = max_loc_G
+    # endX_G = startX_G + kernel_g.shape[1]
+    # endY_G = startY_G + kernel_g.shape[0]
+    #
+    # # draw the bounding box on the image
+    # g_box_image = cv2.rectangle(image, (startX_G, startY_G), (endX_G, endY_G), (0, 255, 0), 3)
+    # # show the output image
+    # # cv2.imshow("Output based on matchTemplate", g_box_image)
+    # cv2.imwrite('res_match_template_GREEN_BoundingBox.tiff', g_box_image)
+    # # cv2.waitKey(0)
 
 '''
     cv::TemplateMatchModes 
@@ -192,8 +213,8 @@ def kernel_runner(image):
 
 if __name__ == '__main__':
     print("Your OpenCV version is: " + cv2.__version__)  
-    # image = cv2.imread("images_10x10/diamond.tif")
-    image = cv2.imread("tic_tac_toe_images/twistCorrectedColoredSquares_Color.tiff")
+    image = cv2.imread("images_10x10/test_box_50x50.tif")
+    # image = cv2.imread("tic_tac_toe_images/twistCorrectedColoredSquares_Color.tiff")
 
     # image = cv2.imread("images_20x20/test_box_20x20.tif")
     # image = image[:, :, 0]
